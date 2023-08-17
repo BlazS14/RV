096ddf1ee39502c497911ac47ba6573023adfa45
blazs14 <gta.blaz.satler@gmail.com>
2023-08-17 20:16:40 +0200

    gr

--- Vaja1/vaja1.py
+++ Vaja1/vaja1.py
@@ -16,7 +16,7 @@ import torchinfo
 import torch_directml
 dml = torch_directml.device()
 torch.set_default_device(dml)
-torch.set_default_dtype(torch.float64)
+torch.set_default_dtype(torch.float32)
 
 
 
@@ -61,6 +61,7 @@ class ImageDataSet(Dataset):
         img = cv2.imread(path)
         img = cv2.resize(img, (320, 240))
         img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        orig_img = img.copy()
         x = random.randint(0+48,320-48)
         y = random.randint(0+48,240-48)
         corners = np.array([[x-32,y-32],[x+32,y-32],[x+32,y+32],[x-32,y+32]], dtype=np.int32)
@@ -70,6 +71,11 @@ class ImageDataSet(Dataset):
         
         transformed_img = transformed_img[y-32:y+32,x-32:x+32]
         
+        corner_offsets = transformed_corners - corners
+        corner_offsets = corner_offsets.flatten()
+        corner_offsets = corner_offsets.astype(np.float32)
+        corner_offsets += 16
+        corner_offsets /= 32
         
         img = img[y-32:y+32,x-32:x+32]
         
@@ -81,7 +87,22 @@ class ImageDataSet(Dataset):
         cv2.destroyAllWindows()'''
         img = cv2.merge((img,transformed_img))
         img = img/255
-        return np.array(img).astype(np.float32),corners,transformed_corners,homography_matrix
+        return np.array(img).astype(np.float32),corners,transformed_corners,homography_matrix,corner_offsets,orig_img
+
+def show_corners_on_image(img,orig_corners,transformed_corners, model_corners):
+    orig_img = img
+    orig_img = cv2.cvtColor(orig_img, cv2.COLOR_GRAY2BGR)
+    orig_corners = orig_corners.astype(np.int32)
+    transformed_corners = transformed_corners.astype(np.int32)
+    model_corners = model_corners.astype(np.int32)
+    
+    cv2.rectangle(orig_img, (orig_corners[0,0],orig_corners[0,1]), (orig_corners[3,0],orig_corners[3,1]), (255,0,0), 1)
+    cv2.rectangle(orig_img, (transformed_corners[0,0],transformed_corners[0,1]), (transformed_corners[3,0],transformed_corners[3,1]), (0,255,0), 1)
+    cv2.rectangle(orig_img, (model_corners[0,0],model_corners[0,1]), (model_corners[3,0],model_corners[3,1]), (0,0,255), 1)
+    cv2.imshow("img",orig_img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
 
 class RMSELoss(nn.Module):
     def __init__(self, eps=1e-6):
@@ -132,13 +153,8 @@ class HomoNet(nn.Module):
         self.regression_head = nn.Linear(512, 8)
         
         self.cls_linear1 = nn.Linear(128*8*8, 8*21)
-        self.cls_softmax =  nn.Softmax()
+        self.cls_softmax = nn.Softmax(dim=2)
         
-        self.fc_cls = nn.Sequential(
-            nn.Linear(128*8*8, 8*21),
-            nn.ReLU(),
-            nn.Linear(512, 8 * num_classes)
-        )
         
         self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
     def forward(self, x):
@@ -161,46 +177,63 @@ class HomoNet(nn.Module):
         #unfold = nn.Unfold(kernel_size=(x.size(2), x.size(3)))
         #x = unfold(x).mean(1)  # Uporabite unfold za povprečno vrednost
         
+        #x = self.cls_linear1(x)
+        #x = torch.reshape(x, (-1,8,21))
+        #x = self.cls_softmax(x)
+        
         x = self.linear1(x)
         x = self.regression_head(x)
+        
         return x
 
 def train(model, loss_fn, optim, epochs, data_gen):
     loss_sum = 0
-    for epoch in range(1,epochs+1):
-        count = 1
-        for img,corners,transformed_corners, homo_matrix in data_gen:
-            
-            #for i in range(len(img)):
-            
-                
-            in_img = torch.moveaxis(img,-1,1)
-            model.zero_grad()
-            model_homo_matrix = model(in_img)
-            ones_tensor = torch.ones((len(img), 1))
-            model_homo_matrix = torch.cat((model_homo_matrix, ones_tensor), dim=1)
-            model_homo_matrix = torch.reshape(model_homo_matrix,(len(img),3,3))
-            #corners_model = torch.reshape(corners_model,(-1,4,2))
-            #corners_orig = corners[i]
-            #model_homo_matrix, _ = cv2.findHomography(corners_orig.numpy(),corners_model.numpy())
-            loss = loss_fn(model_homo_matrix.cpu(),homo_matrix.cpu())
+    eval_count = 0
+    eval_sum = 0
+
+    count = 1
+    for img,corners,transformed_corners, homo_matrix,corner_offsets,orig_img in data_gen:
         
-
-            optim.step()
-            #visualize_flow(flow_out[0].detach().cpu().numpy())
-            #visualize_flow(flow_gt[0].detach().cpu().numpy())
-            loss_sum += loss.item()
+        #for i in range(len(img)):
+        
             
-            if count % 100 == 0:
-                print(f"Epoch: {count}, Loss: {loss_sum/100}")
-                torch.save(model.state_dict(), f"modelv1_{count}.pth")
-                loss_sum = 0
-            count += 1
+        in_img = torch.moveaxis(img,-1,1)
+        model.zero_grad()
+        model_corner_offsets = model(in_img)
+        #ones_tensor = torch.ones((len(img), 1))
+        #model_homo_matrix = torch.cat((model_homo_matrix, ones_tensor), dim=1)
+        #model_homo_matrix = torch.reshape(model_homo_matrix,(len(img),3,3))
+        #corners_model = torch.reshape(corners_model,(-1,4,2))
+        #corners_orig = corners[i]
+        #model_homo_matrix, _ = cv2.findHomography(corners_orig.numpy(),corners_model.numpy())
+        loss = loss_fn(corner_offsets,model_corner_offsets)
+    
+        loss.backward()
+        optim.step()
+        #visualize_flow(flow_out[0].detach().cpu().numpy())
+        #visualize_flow(flow_gt[0].detach().cpu().numpy())
+        loss_sum += loss.item()
+        
+        eval_sum += loss_sum
+        eval_count += 1
+        
+        #for i in range(len(img)):
+        #    show_corners_on_image(orig_img[i].detach().cpu().numpy(),corners[i].detach().cpu().numpy(),transformed_corners[i].detach().cpu().numpy(),corners[i].detach().cpu().numpy()+(torch.reshape(model_corner_offsets,(-1,4,2))[i].detach().cpu().numpy()*32-16))
+        
+        if count % 100 == 0:
+            
+            print(f"Epoch: {count}, Loss: {loss_sum/100}")
+            torch.save(model.state_dict(), f"modelv1_{count}.pth")
+            loss_sum = 0
+        count += 1
 
+    print("Total loss: ",eval_sum/eval_count)
+        
 model = HomoNet()
-#model.load_state_dict(torch.load("model_3380.pth"))
-optim = torch.optim.Adam(model.parameters(), lr=0.0001)
+#model.load_state_dict(torch.load("modelv1_1100.pth"))
+#model.eval()
+optim = torch.optim.Adam(model.parameters(), lr=0.00001)
 data_set = ImageDataSet("C:\\Users\\GTAbl\\Desktop\\RV\\Vaja1\\train2017")
-loss_fn = RMSELoss()
+loss_fn = nn.MSELoss()
 data_gen = DataLoader(data_set, batch_size=64,shuffle=True)
 train(model, loss_fn, optim, 90000, data_gen)
--- Vaja2/vaja2.py
+++ Vaja2/vaja2.py
@@ -27,12 +27,19 @@ def generate_convex_quadrilateral_points(min, max):
 def create_image(height, width):
     img = np.zeros((height,width,3), np.uint8)
     img[:,:] = [random.randint(0, 256),random.randint(0, 256),random.randint(0, 256)]
+    color = img[0,0]
     return img
 def generate_rectangle(img):
     height, width = img.shape[:2]
+    
+    if height <width:
+        max = height-40
+    else:
+        max = width-40
+    
     # generate an image and a random rectangle
-    points = generate_convex_quadrilateral_points(height,width)
-    cv2.drawContours(img, [points], 0, (random.randint(0, 256),random.randint(0, 256),random.randint(0, 256)), -1)
+    points = generate_convex_quadrilateral_points(0,max)
+    cv2.drawContours(img, [np.array(points)], 0, (random.randint(0, 256),random.randint(0, 256),random.randint(0, 256)), -1)
     return img, points
 def generate_triangle(img):
     height, width = img.shape[:2]
@@ -81,167 +88,84 @@ def PSNR(original, compressed):
     max_pixel = 255.0
     psnr = 20 * log10(max_pixel / sqrt(mse))
     return psnr
-def calculate_frame_offsets(dirpath):
-    offests = np.empty((0,3), dtype=np.int32)
-    dirlist = os.listdir(dirpath)
-    for n in range(len(dirlist)):
-                offests = np.append(offests,[[n]],axis=0)
-    return offests
-def get_images(dirpath):
-    files = []
-    for file in os.listdir(dirpath):
-        files.append(dirpath+file)
-    return files
-class ImageDataSet(Dataset):
-    def __init__(self, dirpath):
-        self.dirpath = dirpath
-        self.offsets = calculate_frame_offsets(dirpath)
-    def __len__(self):
-        return len(self.offsets)
-    def __getitem__(self, index):
-        dirlist = os.listdir(self.dirpath)
-        img = cv2.imread(self.dirpath+dirlist[self.offsets[index][0]])
-        img = cv2.resize(img, 320, 240)
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        x = random.randint(0+40,320-40)
-        y = random.randint(0+40,240-40)
-        corners = np.array([[x-32,y-32],[x+32,y-32],[x+32,y+32],[x-32,y+32]], dtype=np.int32)
-        transformed_corners = np.array([[x-32+random.randint(-16,16),y-32+random.randint(-16,16)],[x+32+random.randint(-16,16),y-32+random.randint(-16,16)],[x+32+random.randint(-16,16),y+32+random.randint(-16,16)],[x-32+random.randint(-16,16),y+32+random.randint(-16,16)]], dtype=np.int32)
-        homography_matrix, _ = cv2.findHomography(corners,transformed_corners)
-        transformed_img = cv2.warpPerspective(img, homography_matrix, (320,240))
-        img = np.concatenate((img,transformed_img),axis=1)
-        img = img/255
-        return np.array(img).astype(np.float32),corners,transformed_corners,homography_matrix
-class ResNet(nn.Module):
-    def __init__(self, in_channels, out_channels, stride=1):
-        super(ResNet, self).__init__()
-        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(out_channels)
-        self.relu = nn.ReLU(inplace=True)
-        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=False)
-        self.bn2 = nn.BatchNorm2d(out_channels)
-        self.stride = stride
-        # If the input and output channels don't match, we need to apply a 1x1 convolution to match the dimensions.
-        if in_channels != out_channels or stride != 1:
-            self.shortcut = nn.Sequential(
-                nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=stride, bias=False),
-                nn.BatchNorm2d(out_channels)
-            )
+
+def appy_homography_to_image(img,points):
+    height, width = img.shape[:2]
+    
+    hbuffer = int(height/4)
+    wbuffer = int(width/4)
+    
+    p1 = [random.randint(0, hbuffer), random.randint(0, wbuffer)]
+    p2 = [random.randint(0, hbuffer), random.randint(width-wbuffer, width)]
+    p3 = [random.randint(height-hbuffer, height), random.randint(width-wbuffer, width)]
+    p4 = [random.randint(height-hbuffer, height), random.randint(0, wbuffer)]
+    
+    plist = [p1,p2,p3,p4]
+    
+    for i in range(random.randint(0,3)):
+        tmp = plist.pop(0)
+        plist.append(tmp)
+    
+    homography_matrix = cv2.getPerspectiveTransform(np.float32(plist),np.float32([[0,0],[width,0],[width,height],[0,height]]))
+    
+    img = cv2.warpPerspective(img, homography_matrix, (width,height))
+    
+    #apply homography to points
+    i = 0
+    l = len(points)
+    while i < l:
+        p = np.array([points[i][0],points[i][1],1])
+        p = np.matmul(homography_matrix,p)
+        p = np.array([p[0]/p[2],p[1]/p[2]])
+        #check if point is NOT inside image
+        if p[0] < 0 or p[0] > width or p[1] < 0 or p[1] > height:
+            np.delete(points,i)
+            l -= 1
         else:
-            self.shortcut = nn.Identity()
-    def forward(self, x):
-        residual = self.shortcut(x)
-        out = self.conv1(x)
-        out = self.bn1(out)
-        out = self.relu(out)
-        out = self.conv2(out)
-        out = self.bn2(out)
-        out += residual
-        out = self.relu(out)
-        return out
-class HomoNet(nn.Module):
-    def __init__(self):
-        super(HomoNet, self).__init__()
-        self.resnet1 = ResNet(2, 64)
-        self.maxpool1 = nn.MaxPool2d(kernel_size=2, stride=2)
-        self.resnet2 = ResNet(64, 64)
-        self.linear1 = nn.Linear(64,512)
-        self.resnet3 = ResNet(64, 128)
-        self.resnet4 = ResNet(128, 128)
-        self.regression_head = nn.Linear(128, 8)
-    def forward(self, x):
-        x = self.resnet1(x)
-        x = self.resnet2(x)
-        x = self.maxpool1(x)
-        x = self.resnet2(x)
-        x = self.resnet2(x)
-        x = self.maxpool1(x)
-        x = self.resnet3(x)
-        x = self.resnet4(x)
-        x = self.maxpool1(x)
-        x = self.resnet4(x)
-        x = self.resnet4(x)
-        x = self.linear1(x)
-        x = self.regression_head(x)
-        return x
-class ImageDataSet(Dataset):
-    def __init__(self):
-        mnist_ds = torchvision.datasets.MNIST(
-        root="datasets", train=True, transform=torchvision.transforms.ToTensor(),
-        download=True)
-        print(f"Širina slik: {mnist_ds[0][0].shape[2]}")
-        print(f"Višina slik: {mnist_ds[0][0].shape[1]}")
-        print(f"Število kanalov: {mnist_ds[0][0].shape[0]}")
-        print(f"Število slik: {len(mnist_ds)}")
-        print(f"Podatkovna zbirka: MNIST (http://yann.lecun.com/exdb/mnist/)")
-        n_rows = 5
-        n_cols = 5
-        _, axes = plt.subplots(n_rows, n_cols)
-        for r in range(n_rows):
-            for c in range(n_cols):
-                img, _ = mnist_ds[random.randint(0, len(mnist_ds) - 1)]
-                axes[r, c].imshow(img.permute(1, 2, 0), cmap="gray")
-                axes[r, c].axis("off")
-        plt.tight_layout()
-        plt.show()
-        self.images = mnist_ds
-    def __len__(self):
-        return len(self.images)
-    def __getitem__(self, index):
-        img, _ = self.images[index]
-        img = img/255
-        return img
-def create_identity(num):
-    identity_array = np.empty((0,100), dtype=np.float32)
-    for i in range(num):
-        identity = np.random.standard_normal(100)
-        identity = (identity + abs(np.min(identity)))
-        identity = identity / np.max(identity)
-        identity_array = np.append(identity_array, [identity], axis=0)
-    identity_array = torch.from_numpy(identity_array.astype(np.float32))
-    return identity_array
-def train(discriminator_model, discriminator_optimizer, discriminator_loss_fn, generator_model, generator_optimizer, generator_loss_fn, epochs, batch_size,dataset,static_identity):
-    discriminator_loss_sum = 0
-    generator_loss_sum = 0
-    for epoch in range(1,epochs+1):
-        discriminator_optimizer.zero_grad()
-        data = next(iter(dataset))
-        discriminator_output = discriminator_model(data)
-        discriminator_loss = discriminator_loss_fn(discriminator_output, torch.ones(batch_size,1))
-        discriminator_loss.backward()
-        generator_output = generator_model(create_identity(batch_size))
-        discriminator_output = discriminator_model(generator_output)
-        discriminator_loss = discriminator_loss_fn(discriminator_output, torch.zeros(batch_size,1))
-        discriminator_loss.backward()
-        discriminator_optimizer.step()
-        img = generator_model(static_identity)
-        img = img * 255
-        img = img.detach().numpy()[0,0,:,:]
-        img = img.astype(np.uint8)
-        discriminator_optimizer.zero_grad()
-        generator_optimizer.zero_grad()
-        generator_model.zero_grad()
-        generator_output = generator_model(create_identity(batch_size))
-        discriminator_output = discriminator_model(generator_output)
-        generator_loss = generator_loss_fn(discriminator_output, torch.ones(batch_size,1))
-        generator_loss.backward()
-        generator_optimizer.step()
-        generator_loss_sum += generator_loss.item()
-        discriminator_loss_sum += discriminator_loss.item()
-        if epoch % 10 == 0:
-            print(f"Epoch: {epoch}, Generator loss: {generator_loss_sum/10}, Discriminator loss: {discriminator_loss_sum/10}")
-            generator_loss_sum = 0
-            discriminator_loss_sum = 0
-            cv2.imwrite("img"+str(epoch)+".png",img)
-disc_model = Discriminator()
-disc_optim = torch.optim.Adam(disc_model.parameters(), lr=0.0002, betas=(0.5, 0.999))
-disc_loss_fn = nn.BCELoss()
-gen_model = Generator()
-gen_optim = torch.optim.Adam(gen_model.parameters(), lr=0.0002, betas=(0.5, 0.999))
-gen_loss_fn = nn.BCELoss()
-data_set = ImageDataSet()
-data_gen = DataLoader(data_set, batch_size=128,shuffle=True)
-train(disc_model, disc_optim, disc_loss_fn, gen_model, gen_optim, gen_loss_fn,
-      1000, 128, data_gen,create_identity(1))
-torch.save(disc_model, "model_Discriminator.pt")
-torch.save(gen_model, "model_Generator.pt")
+            i += 1
+    
+    return img, points
+    
+#check if all rgb colors in image differ by a certain amount    
+def get_color_differnece(img,amount):
+
+    #get all unique rgb values in image into a list
+    unique_colors = []
+    for i in range(img.shape[0]):
+        for j in range(img.shape[1]):
+            for u in range(len(unique_colors)):
+                if np.array_equal(img[i,j],unique_colors[u]):
+                    break
+                unique_colors.append(img[i,j])
+                
+    #check if all rgb values differ by a certain amount
+    for i in range(len(unique_colors)):
+        for j in range(len(unique_colors)):
+            if not (abs(unique_colors[i][0]-unique_colors[j][0]) > amount or abs(unique_colors[i][1]-unique_colors[j][1]) > amount or abs(unique_colors[i][2]-unique_colors[j][2]) > amount):
+                return False
+    return True
+
+
+def generate_image(shepe_type,height,width):
+    while True:
+        img = create_image(height,width)
+        if shepe_type == 0:
+            img, points = generate_rectangle(img)
+        elif shepe_type == 1:
+            img, points = generate_triangle(img)
+        elif shepe_type == 2:
+            img, points = generate_star(img)
+        
+        img,points = appy_homography_to_image(img,points)
+        
+        if get_color_differnece(img,50):
+            break
+    return img, points
+    
+    
+    
+while True:
+    img, points = generate_image(random.randint(0,2),320,320)
+    cv2.imshow("img",img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
