97a3ef5e6755037d533413e53da7ebd7ce79dfaf
blazs14 <gta.blaz.satler@gmail.com>
2023-08-17 12:32:10 +0200

    hf

--- Vaja1/vaja1.py
+++ Vaja1/vaja1.py
@@ -12,19 +12,34 @@ from torch.utils.data import Dataset, DataLoader
 from math import log10, sqrt
 from skimage.metrics import structural_similarity as SSIM
 import torchinfo
-def PSNR(original, compressed):
-    mse = np.mean((original - compressed) ** 2)
-    if(mse == 0):  # MSE is zero means no noise is present in the signal .
-                  # Therefore PSNR have no importance.
-        return 100
-    max_pixel = 255.0
-    psnr = 20 * log10(max_pixel / sqrt(mse))
-    return psnr
+
+import torch_directml
+dml = torch_directml.device()
+torch.set_default_device(dml)
+torch.set_default_dtype(torch.float64)
+
+
+
+#write numpy array to file
+def write_to_file(array, filename):
+    with open(filename, 'wb') as f:
+        np.save(f, array)
+        
+#read numpy array from file
+def read_from_file(filename):
+    with open(filename, 'rb') as f:
+        array = np.load(f)
+    return array
+
+#check if file exists
+def file_exists(filename):
+    return os.path.isfile(filename)
+
 def calculate_frame_offsets(dirpath):
-    offests = np.empty((0,3), dtype=np.int32)
+    offests = np.empty((0,1), dtype=np.int32)
     dirlist = os.listdir(dirpath)
     for n in range(len(dirlist)):
-                offests = np.append(offests,[[n]],axis=0)
+                offests = np.append(offests,[[dirlist[n]]],axis=0)
     return offests
 def get_images(dirpath):
     files = []
@@ -34,24 +49,49 @@ def get_images(dirpath):
 class ImageDataSet(Dataset):
     def __init__(self, dirpath):
         self.dirpath = dirpath
-        self.offsets = calculate_frame_offsets(dirpath)
+        if not file_exists(dirpath+"\\offsets.npy"):
+            self.offsets = calculate_frame_offsets(dirpath)
+            write_to_file(self.offsets, dirpath+"\\offsets.npy")
+        else:
+            self.offsets = read_from_file(dirpath+"\\offsets.npy")
     def __len__(self):
         return len(self.offsets)
     def __getitem__(self, index):
-        dirlist = os.listdir(self.dirpath)
-        img = cv2.imread(self.dirpath+dirlist[self.offsets[index][0]])
-        img = cv2.resize(img, 320, 240)
+        path = self.dirpath+"\\"+self.offsets[index][0]
+        img = cv2.imread(path)
+        img = cv2.resize(img, (320, 240))
         img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         x = random.randint(0+48,320-48)
         y = random.randint(0+48,240-48)
         corners = np.array([[x-32,y-32],[x+32,y-32],[x+32,y+32],[x-32,y+32]], dtype=np.int32)
         transformed_corners = np.array([[x-32+random.randint(-16,16),y-32+random.randint(-16,16)],[x+32+random.randint(-16,16),y-32+random.randint(-16,16)],[x+32+random.randint(-16,16),y+32+random.randint(-16,16)],[x-32+random.randint(-16,16),y+32+random.randint(-16,16)]], dtype=np.int32)
         homography_matrix, _ = cv2.findHomography(corners,transformed_corners)
-        transformed_img = cv2.warpPerspective(img, homography_matrix, (320,240))
-        img = np.concatenate((img,transformed_img),axis=1)
+        transformed_img = cv2.warpPerspective(img, homography_matrix, (320,240),flags=cv2.WARP_INVERSE_MAP)
+        
+        transformed_img = transformed_img[y-32:y+32,x-32:x+32]
+        
+        
+        img = img[y-32:y+32,x-32:x+32]
+        
+        '''cv2.imshow("img",img)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+        cv2.imshow("img",transformed_img)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()'''
+        img = cv2.merge((img,transformed_img))
         img = img/255
         return np.array(img).astype(np.float32),corners,transformed_corners,homography_matrix
-    
+
+class RMSELoss(nn.Module):
+    def __init__(self, eps=1e-6):
+        super().__init__()
+        self.mse = nn.MSELoss()
+        self.eps = eps
+        
+    def forward(self,yhat,y):
+        loss = torch.sqrt(self.mse(yhat,y) + self.eps)
+        return loss
     
 class ResNet(nn.Module):
     def __init__(self, in_channels, out_channels, stride=1):
@@ -86,10 +126,21 @@ class HomoNet(nn.Module):
         self.resnet1 = ResNet(2, 64)
         self.maxpool1 = nn.MaxPool2d(kernel_size=2, stride=2)
         self.resnet2 = ResNet(64, 64)
-        self.linear1 = nn.Linear(64,512)
         self.resnet3 = ResNet(64, 128)
         self.resnet4 = ResNet(128, 128)
-        self.regression_head = nn.Linear(128, 8)
+        self.linear1 = nn.Linear(8192,512)
+        self.regression_head = nn.Linear(512, 8)
+        
+        self.cls_linear1 = nn.Linear(128*8*8, 8*21)
+        self.cls_softmax =  nn.Softmax()
+        
+        self.fc_cls = nn.Sequential(
+            nn.Linear(128*8*8, 8*21),
+            nn.ReLU(),
+            nn.Linear(512, 8 * num_classes)
+        )
+        
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
     def forward(self, x):
         x = self.resnet1(x)
         x = self.resnet2(x)
@@ -102,83 +153,54 @@ class HomoNet(nn.Module):
         x = self.maxpool1(x)
         x = self.resnet4(x)
         x = self.resnet4(x)
+        
+        #x = self.avgpool(x)  
+        x = torch.flatten(x, 1) 
+        #x = torch.reshape(x, (-1,128))
+        
+        #unfold = nn.Unfold(kernel_size=(x.size(2), x.size(3)))
+        #x = unfold(x).mean(1)  # Uporabite unfold za povprečno vrednost
+        
         x = self.linear1(x)
         x = self.regression_head(x)
         return x
-class ImageDataSet(Dataset):
-    def __init__(self):
-        mnist_ds = torchvision.datasets.MNIST(
-        root="datasets", train=True, transform=torchvision.transforms.ToTensor(),
-        download=True)
-        print(f"Širina slik: {mnist_ds[0][0].shape[2]}")
-        print(f"Višina slik: {mnist_ds[0][0].shape[1]}")
-        print(f"Število kanalov: {mnist_ds[0][0].shape[0]}")
-        print(f"Število slik: {len(mnist_ds)}")
-        print(f"Podatkovna zbirka: MNIST (http://yann.lecun.com/exdb/mnist/)")
-        n_rows = 5
-        n_cols = 5
-        _, axes = plt.subplots(n_rows, n_cols)
-        for r in range(n_rows):
-            for c in range(n_cols):
-                img, _ = mnist_ds[random.randint(0, len(mnist_ds) - 1)]
-                axes[r, c].imshow(img.permute(1, 2, 0), cmap="gray")
-                axes[r, c].axis("off")
-        plt.tight_layout()
-        plt.show()
-        self.images = mnist_ds
-    def __len__(self):
-        return len(self.images)
-    def __getitem__(self, index):
-        img, _ = self.images[index]
-        img = img/255
-        return img
-def create_identity(num):
-    identity_array = np.empty((0,100), dtype=np.float32)
-    for i in range(num):
-        identity = np.random.standard_normal(100)
-        identity = (identity + abs(np.min(identity)))
-        identity = identity / np.max(identity)
-        identity_array = np.append(identity_array, [identity], axis=0)
-    identity_array = torch.from_numpy(identity_array.astype(np.float32))
-    return identity_array
-def train(discriminator_model, discriminator_optimizer, discriminator_loss_fn, generator_model, generator_optimizer, generator_loss_fn, epochs, batch_size,dataset,static_identity):
-    discriminator_loss_sum = 0
-    generator_loss_sum = 0
+
+def train(model, loss_fn, optim, epochs, data_gen):
+    loss_sum = 0
     for epoch in range(1,epochs+1):
-        discriminator_optimizer.zero_grad()
-        data = next(iter(dataset))
-        discriminator_output = discriminator_model(data)
-        discriminator_loss = discriminator_loss_fn(discriminator_output, torch.ones(batch_size,1))
-        discriminator_loss.backward()
-        generator_output = generator_model(create_identity(batch_size))
-        discriminator_output = discriminator_model(generator_output)
-        discriminator_loss = discriminator_loss_fn(discriminator_output, torch.zeros(batch_size,1))
-        discriminator_loss.backward()
-        discriminator_optimizer.step()
-        img = generator_model(static_identity)
-        img = img * 255
-        img = img.detach().numpy()[0,0,:,:]
-        img = img.astype(np.uint8)
-        discriminator_optimizer.zero_grad()
-        generator_optimizer.zero_grad()
-        generator_model.zero_grad()
-        generator_output = generator_model(create_identity(batch_size))
-        discriminator_output = discriminator_model(generator_output)
-        generator_loss = generator_loss_fn(discriminator_output, torch.ones(batch_size,1))
-        generator_loss.backward()
-        generator_optimizer.step()
-        generator_loss_sum += generator_loss.item()
-        discriminator_loss_sum += discriminator_loss.item()
-        if epoch % 10 == 0:
-            print(f"Epoch: {epoch}, Generator loss: {generator_loss_sum/10}, Discriminator loss: {discriminator_loss_sum/10}")
-            generator_loss_sum = 0
-            discriminator_loss_sum = 0
-            cv2.imwrite("img"+str(epoch)+".png",img)
+        count = 1
+        for img,corners,transformed_corners, homo_matrix in data_gen:
+            
+            #for i in range(len(img)):
+            
+                
+            in_img = torch.moveaxis(img,-1,1)
+            model.zero_grad()
+            model_homo_matrix = model(in_img)
+            ones_tensor = torch.ones((len(img), 1))
+            model_homo_matrix = torch.cat((model_homo_matrix, ones_tensor), dim=1)
+            model_homo_matrix = torch.reshape(model_homo_matrix,(len(img),3,3))
+            #corners_model = torch.reshape(corners_model,(-1,4,2))
+            #corners_orig = corners[i]
+            #model_homo_matrix, _ = cv2.findHomography(corners_orig.numpy(),corners_model.numpy())
+            loss = loss_fn(model_homo_matrix.cpu(),homo_matrix.cpu())
+        
+
+            optim.step()
+            #visualize_flow(flow_out[0].detach().cpu().numpy())
+            #visualize_flow(flow_gt[0].detach().cpu().numpy())
+            loss_sum += loss.item()
+            
+            if count % 100 == 0:
+                print(f"Epoch: {count}, Loss: {loss_sum/100}")
+                torch.save(model.state_dict(), f"modelv1_{count}.pth")
+                loss_sum = 0
+            count += 1
 
 model = HomoNet()
 #model.load_state_dict(torch.load("model_3380.pth"))
 optim = torch.optim.Adam(model.parameters(), lr=0.0001)
-data_set = ImageDataSet("C:\\Users\\GTAbl\\Desktop\\RV\\Vaja3\\data\\train")
-loss_fn = EPELoss()
-data_gen = DataLoader(data_set, batch_size=8,shuffle=True)
-train(model, loss_fn, optim, 500000, data_gen)
+data_set = ImageDataSet("C:\\Users\\GTAbl\\Desktop\\RV\\Vaja1\\train2017")
+loss_fn = RMSELoss()
+data_gen = DataLoader(data_set, batch_size=64,shuffle=True)
+train(model, loss_fn, optim, 90000, data_gen)
